119631d7d8dcd980442b59db355c3455b510f9fa
natalia <t.natalialm@gmail.com>
2023-09-19 21:13:03 -0600

    Archivos listos para trabajar en equipo

--- src/main/java/org/uv/tpcs_practica01/TPCS_Practica01.java
+++ src/main/java/org/uv/tpcs_practica01/TPCS_Practica01.java
@@ -1,17 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package org.uv.tpcs_practica01;
 
-/**
- *
- * @author miran
- */
 public class TPCS_Practica01 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Cambios listos!");
         
         System.out.println("Hello I'm David");
     }
